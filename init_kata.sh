47490205b8ef04860998ffa5096c974374b7290c
GradedJestRisk <graded.jest.risk@gmail.com>
2019-06-16 10:14:46 +0200

    Fix init_kata.sh to output no errors

--- init_kata.sh
+++ init_kata.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 node_modules_directory=node_modules
 
-echo "--------- Deleting package previous install, (if any) -------"
+echo "--------- Deleting package previous install (if any) -------"
 if [ -d "$node_modules_directory" ]; then
   rm -rf $node_modules_directory
 fi
 
-echo "--------- Remove remote (git and npm)  -------"
-
-if [git ls-remote origin]; then ..
+echo "--------- Remove remote in git  -------"
+if git ls-remote origin >/dev/null 2>&1; then 
 	git remote rm origin
 fi
 
+echo "--------- Remove remote in npm  -------"
 # TODO: suppress remote directly in file (avoid duplicate package.json)
 noremote_file=package.json.noremote
 if [ -d "$noremote_file" ]; then
   mv $noremote_file package.json
 fi
 
-git add --all
-git commit -m "Remove remote in package.json"
+#git add --all
+#git commit -m "Remove remote in package.json"
 
 echo "--------- Installing node modules in package  -------"
 npm install
